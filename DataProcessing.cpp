6ed2771ecf9b15c81f106bc2779d56babcfa315d
MichaelRofail <18582967+MichaelRofail@users.noreply.github.com>
2017-11-26 20:33:55 +0100

    fixed generateXYZ

--- DataProcessing.cpp
+++ DataProcessing.cpp
@@ -5,19 +5,19 @@ void DataProcessing::generateXYZ(PointCloud<PointXYZ>& cloud, float* arr, int ar
     for(int i = 0; i < arrSize; i++){
         if(arr[i] > 0)numOfPoints++;
     }
+    int angle;
     cloud.width += numOfPoints;
     cloud.height = 1;
     cloud.points.resize (cloud.width * cloud.height);//resize the cloud to make space for more points
 
-    for(int j = 0, i = (cloud.width - numOfPoints); i < cloud.width && j < arrSize;){
+    for(int j = 0, i = (cloud.width - numOfPoints); i < cloud.width && j < arrSize; j++){
         if(arr[j] > 0){//if a point in the array not -1 add it to the cloud
-            cloud.points[i].x = arr[j]*cos((360*currentStep)/numOfSteps);
+            angle = (360*currentStep)/numOfSteps;
+            cloud.points[i].x = arr[j]*cos(angle);
             cloud.points[i].y = arrSize - j;
-            cloud.points[i].z = sqrt(arr[j] * arr[j] - cloud.points[i].x * cloud.points[i].x);
-            j++;
+            if(angle < 180)cloud.points[i].z = -1 * sqrt(arr[j] * arr[j] - cloud.points[i].x * cloud.points[i].x);
+            else cloud.points[i].z = sqrt(arr[j] * arr[j] - cloud.points[i].x * cloud.points[i].x);
             i++;
-        }else{
-            j++;
         }
     }
 }
