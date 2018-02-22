3863d5e3ddd64f1406512896a1fac95cc46eb1a2
MichaelRofail <18582967+MichaelRofail@users.noreply.github.com>
2018-02-22 01:46:10 +0100

    added surface reconstruction and fixed conflicts

---
+++ SurfaceReconstruct.cpp
@@ -0,0 +1,29 @@
+#include "SurfaceReconstruct.h"
+
+pcl::PolygonMesh SurfaceReconstruct::reconstruct(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
+    // Normal estimation
+    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud (cloud);
+	n.setInputCloud (cloud);
+	n.setSearchMethod (tree);
+	n.setKSearch (20);
+	n.compute (*normals);
+
+    for(int i = 0; i < normals->size(); ++i){
+	    normals->points[i].normal_x *= -1;
+		normals->points[i].normal_y *= -1;
+		normals->points[i].normal_z *= -1;
+	}
+    
+    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+    concatenateFields (*cloud, *normals, *cloud_with_normals);
+    pcl::Poisson<pcl::PointNormal> poisson;
+    poisson.setDepth(POISSON_DEPTH);
+    poisson.setInputCloud(cloud_with_normals);
+    pcl::PolygonMesh mesh;
+    poisson.reconstruct(mesh);
+    //pcl::io::saveOBJFile("model.obj", this->mesh);
+    return mesh;
+}
---
+++ SurfaceReconstruct.h
@@ -0,0 +1,18 @@
+#ifndef SurfaceReconstruct_H
+#define SurfaceReconstruct_H
+
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/io/obj_io.h>
+#include <pcl/surface/poisson.h>
+
+#define POISSON_DEPTH 7
+
+class SurfaceReconstruct{
+    public:
+    static pcl::PolygonMesh reconstruct(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
+};
+
+#endif
