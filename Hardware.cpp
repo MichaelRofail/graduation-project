3973092562df893b292b669f8b0c12cef1dd35f0
MichaelRofail <18582967+MichaelRofail@users.noreply.github.com>
2018-02-26 15:21:32 +0100

    fixed the hardware files no build

---
+++ Hardware.cpp
@@ -0,0 +1,66 @@
+#include "Hardware.h"
+
+int Hardware::motorStep(){
+
+	static unsigned char count = 0;
+
+	if(count == 0 ){
+		digitalWrite(ENABLE, LOW);
+		digitalWrite(MOTOR_PIN1, LOW);
+		digitalWrite(MOTOR_PIN2, HIGH);
+		digitalWrite(MOTOR_PIN3, LOW);
+		digitalWrite(MOTOR_PIN4, HIGH);
+	count ++;
+	}else if(count == 1){
+		digitalWrite(ENABLE, LOW);
+		digitalWrite(MOTOR_PIN1, HIGH);
+		digitalWrite(MOTOR_PIN2, LOW);
+		digitalWrite(MOTOR_PIN3, LOW);
+		digitalWrite(MOTOR_PIN4, HIGH);
+		count ++;
+	}else if(count == 2){
+		digitalWrite(ENABLE, LOW);
+		digitalWrite(MOTOR_PIN1, HIGH);
+		digitalWrite(MOTOR_PIN2, LOW);
+		digitalWrite(MOTOR_PIN3, HIGH);
+		digitalWrite(MOTOR_PIN4, LOW);
+		count ++;
+	}else{
+		digitalWrite(ENABLE, LOW);
+		digitalWrite(MOTOR_PIN1, LOW);
+		digitalWrite(MOTOR_PIN2, HIGH);
+		digitalWrite(MOTOR_PIN3, HIGH);
+		digitalWrite(MOTOR_PIN4, LOW);
+		count =0;
+	}
+	delay(MOTOR_DELAY);
+}
+
+
+void Hardware::motorInit(){
+
+    wiringPiSetup() //when initialize wiring failed,print messageto screen
+	pinMode(MOTOR_PIN1, OUTPUT);
+	pinMode(MOTOR_PIN2, OUTPUT);
+	pinMode(MOTOR_PIN3, OUTPUT);
+	pinMode(MOTOR_PIN4, OUTPUT);
+	pinMode(ENABLE, OUTPUT);
+}
+
+void Hardware::laserOn(){
+
+	pinMode(LASER_PIN, OUTPUT);
+	digitalWrite(LASER_PIN, HIGH);	     
+    return 0;
+}
+
+void Hardware::laserOff(){
+
+	pinMode(LASER_PIN, OUTPUT);
+	digitalWrite(LASER_PIN, LOW);	     
+    return 0;
+}
+
+
+
+
---
+++ Hardware.h
@@ -0,0 +1,20 @@
+#include <wiringPi.h>
+
+// connect the pins from GPIO0 TO GPIO5 
+#define MOTOR_PIN1    0 // IN1
+#define MOTOR_PIN2    1// IN2
+#define MOTOR_PIN3    2 // IN3
+#define MOTOR_PIN4    3 //IN4
+
+#define ENABLE    4// ENABLE PIN
+#define LASER_PIN   5
+#define MOTOR_DELAY 500
+
+
+class Hardware{
+public: 
+    static void motorStep() ;
+    static void motorInit();
+    static void laserOn();
+    static void laserOff();
+};
