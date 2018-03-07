b64a4498c70f4d53a480f6106347e2e7801a73bc
MichaelRofail <18582967+MichaelRofail@users.noreply.github.com>
2018-03-07 15:33:33 +0100

    fixed int to float bug

--- DataProcessing.cpp
+++ DataProcessing.cpp
@@ -5,12 +5,12 @@ void DataProcessing::generateXYZ(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud
     for(int i = 0; i < arrSize; i++){
         if(arr[i] > 0)numOfPoints++;
     }
-    int angle;
+
     cloud->width += numOfPoints;
     cloud->height = 1;
     cloud->points.resize (cloud->width * cloud->height);//resize the cloud to make space for more points
 
-    angle = (360 * currentStep) / numOfSteps;
+    float angle = (360 * currentStep) / numOfSteps;
     angle = (angle / 180) * 3.14159265; //convert degree to rad
     float cosAngle = cos(angle), sinAngle = sin(angle);
     
