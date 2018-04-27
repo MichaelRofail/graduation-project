26b96b7b1b1aaad45d3e8b529ff95443dbf9bcf1
MichaelRofail <18582967+MichaelRofail@users.noreply.github.com>
2018-04-27 23:39:41 +0200

    used otsu for thresholding and improved extractPoints

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -3,43 +3,48 @@
 using namespace cv;
 
 Mat ImageProcessing::extractLaser(const Mat& laserFrame, const Mat& frame){
-    Mat gray, output, equalised, subframe;
+    Mat gray, output, equalised, subframe, thresh;
     absdiff(laserFrame, frame, subframe);//subtract the normal frame from the laser frame
 
     cvtColor(subframe, gray, COLOR_BGR2GRAY);
-    equalizeHist(gray , equalised);
-     
-    threshold(equalised, output, LASER_THRESHOLD, 255, THRESH_BINARY);
+    
+    GaussianBlur(gray, gray, Size(5,5), 0, 0);//blur to improve otsu's thresh
 
-    //openening morphology to get rid of stray pixels
-    int morph_size = 2;
-    Mat element = getStructuringElement( 1, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( morph_size, morph_size ) );
-    morphologyEx(output, output, MORPH_OPEN, element);
+    threshold(gray, thresh, 0, 255, THRESH_BINARY+THRESH_OTSU);
+
+    medianBlur(thresh, output, MEDIAN_SIZE);
     return output;
 }
 
 Mat ImageProcessing::crop(Mat& input){
 
-    Mat output = input(Rect((input.cols/3) + 20, input.rows/2, (input.cols/3) + 7, (input.rows/2)- 30));
+    //Mat output = input(Rect((input.cols/3) + 20, input.rows/2 - 50, (input.cols/3) + 7, (input.rows/2) + 30));
+    Mat output = input(Rect(630, 430, 268, 490));
     return output;
 }
 
 void ImageProcessing::extractPoints(Mat& inputMat, float* arr){
     bool found;
+    int start , end;
     for( int i = 0; i < inputMat.rows; ++i){
         found = false;
         for( int j = 0; j < inputMat.cols; ++j ){
-            //if a pixel is white check next pixel if it is white too write the point in the array
-            if(inputMat.at<uchar>(i,j) == 255){
-                if(j < inputMat.cols - 1){
-                    if(inputMat.at<uchar>(i,j+1)){
-                        arr[i] = j;
-                        found = true;
-                    }
-                }
-            }    
+            
+            //if a pixel is white set found to true and save the location to start
+            if((found == false)&&(inputMat.at<uchar>(i,j) == 255)){
+                start = j;
+                end = start; //if no end is found 
+                found = true;
+            }
+            //end location where the first black point is found    
+            if((found == true) && (inputMat.at<uchar>(i,j) == 0)){
+                end = j-1;
+                break;
+            }
         }
+        //the point found is the average between the first and last white points found
+        if(found == true)arr[i] = (start + end)/2;
         //if no point is found on a line put -1 in the array
-        if(found == false)arr[i] = -1;
+        else arr[i] = -1;
     }
 }
