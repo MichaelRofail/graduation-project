6beaa6edda5de0a8d2c8092cf7f55ccd78835873
MichaelRofail <18582967+MichaelRofail@users.noreply.github.com>
2018-06-07 16:16:57 +0200

    comments

--- ImageProcessing.h
+++ ImageProcessing.h
@@ -8,8 +8,8 @@
 
 class ImageProcessing{
 public:
-    static cv::Mat extractLaser(const cv::Mat& laserFrame,const cv::Mat& frame);
-    static cv::Mat crop(cv::Mat& input, int top, int middleCropConstant, int BottomCrop);//crops the image to include on the object
+    static cv::Mat extractLaser(const cv::Mat& laserFrame,const cv::Mat& frame);//turns 2 images into a binary image of the laser
+    static cv::Mat crop(cv::Mat& input, int top, int middleCropConstant, int BottomCrop);//crops the image around the object
     static void extractPoints(cv::Mat&, float* arr, float laserAngle);//extract points from each line into an array   
     static int getTopCrop(cv::Mat& img1, cv::Mat& img2);
 };
