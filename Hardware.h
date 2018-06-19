d50206922ea8bc88abda4609bdda879c9f3ac054
MichaelRofail <18582967+MichaelRofail@users.noreply.github.com>
2018-06-19 13:39:41 +0200

    new pins

--- Hardware.h
+++ Hardware.h
@@ -1,14 +1,14 @@
 #include <wiringPi.h>
 
 // connect the pins from GPIO0 TO GPIO5 
-#define MOTOR_PIN1    0 // IN1
-#define MOTOR_PIN2    1 // IN2
-#define MOTOR_PIN3    2 // IN3
-#define MOTOR_PIN4    3 // IN4
-#define ENABLE        4 // ENABLE PIN
+#define MOTOR_PIN1    10 // IN1
+#define MOTOR_PIN2    11 // IN2
+#define MOTOR_PIN3    12 // IN3
+#define MOTOR_PIN4    13 // IN4
+#define ENABLE        14 // ENABLE PIN
 
-#define LASER1_PIN   5
-#define LASER2_PIN   6
+#define LASER1_PIN   15
+#define LASER2_PIN   16
 #define MOTOR_DELAY 500
 
 
