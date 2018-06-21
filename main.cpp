59102b3174ca185baed3c0d2b2326a303e593b38
MichaelRofail <18582967+MichaelRofail@users.noreply.github.com>
2018-06-21 22:00:09 +0200

    changed offset angle

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@
 #define LASER1_ANGLE (0.5236)
 #define LASER2_ANGLE (0.5399)
 #define SMOOTHING_SEARCH_RADIUS 15
-#define BOTTOM_CROP 10
+#define BOTTOM_CROP 100
 #define MIDDLE_CROP_CONSTANT 0
-#define LASER2_OFFSET 119.063
+#define LASER2_OFFSET 60.9375
 
 using namespace std;
 
@@ -80,9 +80,9 @@ int main(void){
     cloud->is_dense = false;
     for(int i = 0; i < NUM_OF_STEPS; i++){//post processing loop that extracts points from frames
         ImageProcessing::extractPoints(croppedL1[i],arr, LASER1_ANGLE);
-        DataProcessing::generateXYZ(cloud, arr, croppedL1[i].rows, i, NUM_OF_STEPS, 0);
-        //ImageProcessing::extractPoints(croppedL2[i],arr, LASER2_ANGLE);
-        //DataProcessing::generateXYZ(cloud, arr, croppedL2[i].rows, i, NUM_OF_STEPS, LASER2_OFFSET);
+        DataProcessing::generateXYZ(cloud, arr, croppedL1[0].rows, i, NUM_OF_STEPS, 0);
+        ImageProcessing::extractPoints(croppedL2[i],arr, LASER2_ANGLE);
+        DataProcessing::generateXYZ(cloud, arr, croppedL2[0].rows, i, NUM_OF_STEPS, LASER2_OFFSET);
     }
     pcl::io::savePCDFileASCII ("my_point_cloud.pcd", *cloud);//save original cloud
     pcl::PointCloud<pcl::PointNormal> cloudNormals = SurfaceReconstruct::smooth(cloud, SMOOTHING_SEARCH_RADIUS);
